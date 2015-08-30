0b7c3dfceceb4172b3684409bd8f2a57a61ed518
Conrad Taylor <conradwt@gmail.com>
2015-08-29 18:55:02 -0700

    Fix the development, production, and test environment files.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.default_url_options = { host: 'https://immense-coast-4160.herokuapp.com' }
 end
